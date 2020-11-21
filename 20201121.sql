213e33ae4c9015cf6c61b53cccc888204642d118
Edīte <Amarilda@users.noreply.github.com>
2020-11-21 13:39:18 +0100

    Add files via upload
    My stats would be better if I would actually manage to write T-SQL in T-SQL section instead of Mysql :D

---
+++ 20201121.sql
@@ -0,0 +1,171 @@
+/* 180. Consecutive Numbers
+Write a SQL query to find all numbers that appear at least three times consecutively.
++----+-----+
+| Id | Num |
++----+-----+
+| 1  |  1  |
+| 2  |  1  |
+| 3  |  1  |
+| 4  |  2  |
+| 5  |  1  |
+| 6  |  2  |
+| 7  |  2  |
++----+-----+
+For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
+
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+*/
+
+select distinct L1.Num as ConsecutiveNums
+from logs L1
+
+join logs L2
+on L1.Id+1 = L2.ID
+join logs L3
+on L1.Id+2 = L3.ID
+
+where L1.num = L2.num and L1.num = L3.num
+
+
+/* 184. Department Highest Salary
+The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, your SQL query should return the following rows (order of rows does not matter).
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+*/
+
+select department.Name as Department
+,TMP1.name as Employee
+,TMP1.salary
+
+from (
+
+SELECT Name, salary, DepartmentId, max(salary) over (partition by DepartmentId) as maximum
+from employee
+    
+    )TMP1
+    
+left join  department
+on TMP1.DepartmentId = department.id
+
+where tmp1.salary = tmp1.maximum and department.Name is not null
+
+/* 262. Trips and Users
+The Trips table holds all taxi trips. Each trip has a unique Id, while Client_Id and Driver_Id are both foreign keys to the Users_Id at the Users table. Status is an ENUM type of (‘completed’, ‘cancelled_by_driver’, ‘cancelled_by_client’).
+
++----+-----------+-----------+---------+--------------------+----------+
+| Id | Client_Id | Driver_Id | City_Id |        Status      |Request_at|
++----+-----------+-----------+---------+--------------------+----------+
+| 1  |     1     |    10     |    1    |     completed      |2013-10-01|
+| 2  |     2     |    11     |    1    | cancelled_by_driver|2013-10-01|
+| 3  |     3     |    12     |    6    |     completed      |2013-10-01|
+| 4  |     4     |    13     |    6    | cancelled_by_client|2013-10-01|
+| 5  |     1     |    10     |    1    |     completed      |2013-10-02|
+| 6  |     2     |    11     |    6    |     completed      |2013-10-02|
+| 7  |     3     |    12     |    6    |     completed      |2013-10-02|
+| 8  |     2     |    12     |    12   |     completed      |2013-10-03|
+| 9  |     3     |    10     |    12   |     completed      |2013-10-03| 
+| 10 |     4     |    13     |    12   | cancelled_by_driver|2013-10-03|
++----+-----------+-----------+---------+--------------------+----------+
+The Users table holds all users. Each user has an unique Users_Id, and Role is an ENUM type of (‘client’, ‘driver’, ‘partner’).
+
++----------+--------+--------+
+| Users_Id | Banned |  Role  |
++----------+--------+--------+
+|    1     |   No   | client |
+|    2     |   Yes  | client |
+|    3     |   No   | client |
+|    4     |   No   | client |
+|    10    |   No   | driver |
+|    11    |   No   | driver |
+|    12    |   No   | driver |
+|    13    |   No   | driver |
++----------+--------+--------+
+Write a SQL query to find the cancellation rate of requests made by unbanned users (both client and driver must be unbanned) between Oct 1, 2013 and Oct 3, 2013. The cancellation rate is computed by dividing the number of canceled (by client or driver) requests made by unbanned users by the total number of requests made by unbanned users.
+
+For the above tables, your SQL query should return the following rows with the cancellation rate being rounded to two decimal places.
+
++------------+-------------------+
+|     Day    | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 |       0.33        |
+| 2013-10-02 |       0.00        |
+| 2013-10-03 |       0.50        |
++------------+-------------------+*/
+
+select Request_at as Day
+, cast(cast(sum(case when Status in ('cancelled_by_driver', 'cancelled_by_client') then 1 else 0 end)as decimal(5,2))/ count(*) as decimal(5,2)) as 'Cancellation Rate'
+
+from Trips
+
+left join users U1
+on Trips.Client_Id =U1.Users_Id
+left join users U2 
+on Trips.Driver_Id =U2.Users_Id
+
+where U1.Banned = 'No' and U2.Banned = 'No'
+and Request_at >=  '2013-10-01' and Request_at <='2013-10-03'
+
+group by Request_at
+
+/* 596. Classes More Than 5 Students
+There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+*/
+
+
+select TMP1.class 
+from (
+select distinct student, class
+from Courses) TMP1
+group by TMP1.class
+having count(*)>4
